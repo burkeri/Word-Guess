16105e15b091713d4ba85fc16d6fc9b0f0ccb74d
Richelle Burke <richelleburke@Richelles-MacBook-Pro.local>
2018-11-08 17:46:11 -0500

    v1: html and bootstrap css complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+//CONTROLS
+// key stroke to start the game
+// way to record user key strokes
+// way to check if those key strokes are correct
+// a set number of guesses
+// record of wins and losses
+
+// MUSICALS / ANSWERS
+
+// ANSWERS
+
+var answers = ["Hello Dolly", "Book"];
+
+var musicals = {
+    
+}
+
+// Hello, Dolly!
+var dolly ={
+    name: "Hello Dolly",
+    bWay: "1964",
+    opNum: "Call on Dolly"
+}
+// Book of Mormon!
+var dolly ={
+    name: "The Book of Mormon",
+    bWay: "2011",
+    opNum: "Hello"
+}
+// Next to Normal
+var dolly ={
+    name: "Next to Normal",
+    bWay: "2009",
+    opNum: "Just Another Day"
+}
+// Dear Evan Hansen
+var dolly ={
+    name: "Dear Evan Hansen",
+    bWay: "2016",
+    opNum: "Anybody Have a Map?"
+}
+// Ragtime
+var ragtime ={
+    name: "Ragtime",
+    bWay: "1998",
+    opNum: "Ragtime"
+}
+// The Music Man
+var music ={
+    name: "The Music Man",
+    bWay: "1957",
+    opNum: "Rock Island"
+}
+// Sweeney Todd
+var music ={
+    name: "Sweeney Todd: The Demon Barber of Fleet Street",
+    bWay: "1979",
+    opNum: "The Ballad of Sweeney Todd"
+}
+
+
+// Singing in the Rain
+var rain = {}
+// Hamilton
+var hamilton = {}
+// Anything Goes
+var anything = {}
